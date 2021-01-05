114c6c81e39268fb1c749100460a084ed8a2e512
Olatunji Yusuf <olatunjiyso@gmail.com>
2021-01-05 17:08:28 +0100

    prepare prod environment

--- config/env/production.js
+++ config/env/production.js
@@ -47,8 +47,8 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     default: {
-      // adapter: 'sails-mysql',
-      // url: 'mysql://user:password@host:port/database',
+       adapter: 'sails-postgresql',
+       url: process.env.DATABASE_URL,
       //--------------------------------------------------------------------------
       //  /\   To avoid checking it in to version control, you might opt to set
       //  ||   sensitive credentials like `url` using an environment variable.
@@ -325,7 +325,7 @@ module.exports = {
     * (https://sailsjs.com/config/http)                                        *
     *                                                                          *
     ***************************************************************************/
-    // trustProxy: true,
+    trustProxy: true,
 
   },
 
